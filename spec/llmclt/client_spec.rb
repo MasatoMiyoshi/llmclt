c90da65d8d1f2d71d9c1bd847ed6bb68bbcc6f37
MiyoshiLight <mirasi1h@gmail.com>
2025-03-02 22:03:28 +0900

    Add batch request feature

--- spec/llmclt/client_spec.rb
+++ spec/llmclt/client_spec.rb
@@ -5,13 +5,6 @@ RSpec.describe Llmclt::Client do
   let!(:location_id) { 'asia-northeast1' }
   let!(:model) { 'gemini-1.5-pro-001' }
   let!(:service_account_json) { File.read('spec/fixtures/service_account.json') }
-  let!(:prompt) { 'Hello! How are you?' }
-  let!(:histories) do
-    [
-      { role: 'user', text: 'This is Tom.' },
-      { role: 'model', text: 'OK!' }
-    ]
-  end
   let!(:client) do
     described_class.new(
       project_id: project_id,
@@ -40,19 +33,29 @@ RSpec.describe Llmclt::Client do
     )
   end
 
+  before do
+    stub_request(:post, 'https://www.googleapis.com/oauth2/v4/token')
+      .to_return(
+        headers: { 'Content-Type' => 'application/json' },
+        body: File.read('spec/fixtures/oauth2_response.json')
+      )
+  end
+
   after do
     client.shutdown
     client_with_config.shutdown
   end
 
   describe '#request' do
-    before do
-      stub_request(:post, 'https://www.googleapis.com/oauth2/v4/token')
-        .to_return(
-          headers: { 'Content-Type' => 'application/json' },
-          body: File.read('spec/fixtures/oauth2_response.json')
-        )
+    let!(:prompt) { 'Hello! How are you?' }
+    let!(:histories) do
+      [
+        { role: 'user', text: 'This is Tom.' },
+        { role: 'model', text: 'OK!' }
+      ]
+    end
 
+    before do
       stub_request(:post, "https://#{location_id}-aiplatform.googleapis.com/v1/projects/#{project_id}/locations/#{location_id}/publishers/google/models/#{model}:generateContent")
         .with(headers: { 'Content-type': 'application/json' })
         .to_return(
@@ -108,6 +111,47 @@ RSpec.describe Llmclt::Client do
     it 'gets a response' do
       jsonl = client.chat_jsonl(prompts)
       expect(jsonl).to eq(expected_jsonl)
+    end
+  end
+
+  describe '#batch_request' do
+    let!(:batch_job_name) { 'BATCH_JOB_NAME' }
+    let!(:gcs_input_uri) { 'gs://bucketname/sample/file.jsonl' }
+    let!(:gcs_output_uri) { 'gs://bucketname/sample' }
+
+    before do
+      stub_request(:post, "https://#{location_id}-aiplatform.googleapis.com/v1/projects/#{project_id}/locations/#{location_id}/batchPredictionJobs")
+        .with(headers: { 'Content-type': 'application/json' })
+        .to_return(
+          headers: { 'Content-Type' => 'application/json' },
+          body: File.read('spec/fixtures/gemini_batch_response.json')
+        )
+    end
+
+    it 'gets a response' do
+      response = client.batch_request(batch_job_name, gcs_input_uri, gcs_output_uri)
+      expect(response.success?).to be(true)
+      expect(response.batch_job_id).to eq('BATCH_JOB_ID')
+      expect(response.state).to eq('JOB_STATE_PENDING')
+    end
+  end
+
+  describe '#batch_state_request' do
+    let!(:batch_job_id) { 'BATCH_JOB_ID' }
+
+    before do
+      stub_request(:get, "https://#{location_id}-aiplatform.googleapis.com/v1/projects/#{project_id}/locations/#{location_id}/batchPredictionJobs/#{batch_job_id}")
+        .with(headers: { 'Content-type': 'application/json' })
+        .to_return(
+          headers: { 'Content-Type' => 'application/json' },
+          body: File.read('spec/fixtures/gemini_batch_response.json')
+        )
+    end
+
+    it 'gets a response' do
+      response = client.batch_state_request(batch_job_id)
+      expect(response.success?).to be(true)
+      expect(response.state_succeeded?).to eq(false)
     end
   end
 end
