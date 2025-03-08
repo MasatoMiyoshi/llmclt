b81a5aedca7715c4343f8a2806d46809cf125be2
MiyoshiLight <mirasi1h@gmail.com>
2025-03-08 18:57:00 +0900

    Add request jsonl output feature

--- lib/llmclt/request/chat.rb
+++ lib/llmclt/request/chat.rb
@@ -6,53 +6,7 @@ module Llmclt
       private
 
       def build_request_json
-        req = {}
-        req.merge!(build_request_contents(@prompt, @histories))
-        req.merge!(build_request_system_instruction)
-        req.to_json
-      end
-
-      def build_request_contents(prompt, histories)
-        content = { contents: [] }
-        content.merge!(@config.safety_config.build_request_content)
-        content.merge!(@config.generation_config.build_request_content)
-        content[:contents].concat(build_request_history_contents(histories))
-        content[:contents] << build_request_user_prompt(prompt)
-        content
-      end
-
-      def build_request_history_contents(histories)
-        histories.map do |history|
-          {
-            role: history[:role],
-            parts: [{ text: history[:text] }]
-          }
-        end
-      end
-
-      def build_request_user_prompt(prompt)
-        {
-          role: 'user',
-          parts: [
-            {
-              text: prompt
-            }
-          ]
-        }
-      end
-
-      def build_request_system_instruction
-        return {} if @config.system_instruction_prompt.nil? || @config.system_instruction_prompt.empty?
-
-        {
-          systemInstruction: {
-            parts: [
-              {
-                text: @config.system_instruction_prompt
-              }
-            ]
-          }
-        }
+        Llmclt::Syntax::Chat.new(@prompt, @config, histories: @histories).to_json
       end
     end
   end
--- spec/llmclt/client_spec.rb
+++ spec/llmclt/client_spec.rb
@@ -1,46 +1,51 @@
 # frozen_string_literal: true
 
 RSpec.describe Llmclt::Client do
-  describe '#request' do
-    let!(:project_id) { 'aaa-bbb-123456' }
-    let!(:location_id) { 'asia-northeast1' }
-    let!(:model) { 'gemini-1.5-pro-001' }
-    let!(:service_account_json) { File.read('spec/fixtures/service_account.json') }
-    let!(:prompt) { 'Hello! How are you?' }
-    let!(:histories) do
-      [
-        { role: 'user', text: 'This is Tom.' },
-        { role: 'model', text: 'OK!' }
-      ]
-    end
-    let!(:client) do
-      described_class.new(
-        project_id: project_id,
-        location_id: location_id,
-        model: model,
-        service_account_json: service_account_json
-      )
-    end
-    let!(:client_with_config) do
-      described_class.new(
-        project_id: project_id,
-        location_id: location_id,
-        model: model,
-        service_account_json: service_account_json,
-        system_instruction_prompt: "I'm a helpful assistant.",
-        safety_settings: [
-          {
-            category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
-            threshold: 'BLOCK_LOW_AND_ABOVE'
-          }
-        ],
-        generation_config: {
-          temperature: 1.0,
-          top_p: 0.95
+  let!(:project_id) { 'aaa-bbb-123456' }
+  let!(:location_id) { 'asia-northeast1' }
+  let!(:model) { 'gemini-1.5-pro-001' }
+  let!(:service_account_json) { File.read('spec/fixtures/service_account.json') }
+  let!(:prompt) { 'Hello! How are you?' }
+  let!(:histories) do
+    [
+      { role: 'user', text: 'This is Tom.' },
+      { role: 'model', text: 'OK!' }
+    ]
+  end
+  let!(:client) do
+    described_class.new(
+      project_id: project_id,
+      location_id: location_id,
+      model: model,
+      service_account_json: service_account_json
+    )
+  end
+  let!(:client_with_config) do
+    described_class.new(
+      project_id: project_id,
+      location_id: location_id,
+      model: model,
+      service_account_json: service_account_json,
+      system_instruction_prompt: "I'm a helpful assistant.",
+      safety_settings: [
+        {
+          category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+          threshold: 'BLOCK_LOW_AND_ABOVE'
         }
-      )
-    end
+      ],
+      generation_config: {
+        temperature: 1.0,
+        top_p: 0.95
+      }
+    )
+  end
 
+  after do
+    client.shutdown
+    client_with_config.shutdown
+  end
+
+  describe '#request' do
     before do
       stub_request(:post, 'https://www.googleapis.com/oauth2/v4/token')
         .to_return(
@@ -61,11 +66,6 @@ RSpec.describe Llmclt::Client do
           headers: { 'Content-Type' => 'application/json' },
           body: File.read('spec/fixtures/gemini_stream_response.json')
         )
-    end
-
-    after do
-      client.shutdown
-      client_with_config.shutdown
     end
 
     context 'when stream is true' do
@@ -98,6 +98,16 @@ RSpec.describe Llmclt::Client do
         expect(response.success?).to be(true)
         expect(response.text).to eq("I'm Fine.")
       end
+    end
+  end
+
+  describe '#chat_jsonl' do
+    let!(:prompts) { ['Hello! How are you?', 'This is Tom.'] }
+    let!(:expected_jsonl) { File.read('spec/fixtures/gemini_request.jsonl') }
+
+    it 'gets a response' do
+      jsonl = client.chat_jsonl(prompts)
+      expect(jsonl).to eq(expected_jsonl)
     end
   end
 end
